055c77824278461d2670f2d35cbad48bbd9ea59d
Ryan evertz <r.evertz@eyeonim.nl>
2025-04-13 14:32:21 +0200

    Rename + Added filter used for archive.org filtering

---
+++ MovieScraper/MovieScraper.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieScraper
+{
+    public class MovieScraper
+    {
+        public const string filter = @"mediatype:(movies) and collection:(moviesandfilms) AND genre:["" TO *]";
+        
+        public MovieScraper() 
+        { 
+            
+        }
+    }
+}
--- ScraperConsoleTest/Program.cs
+++ ScraperConsoleTest/Program.cs
@@ -1,6 +1,17 @@
-﻿internal class Program
+﻿using Microsoft.Extensions.Logging;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.SetMinimumLevel(LogLevel.Information);
+        });
+
+        var logger = loggerFactory.CreateLogger<MovieScraper.MovieScraper>();
+        var scraper = new MovieScraper.MovieScraper(logger);
+
+        await scraper.ScrapeMoviesAsync("science fiction", 5);
     }
 }
